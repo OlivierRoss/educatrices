cae6fc5a9cfa8c3f9e6ab623dc7ce77372170372
Olivier Ross <info@olivierross.com>
2023-11-27 10:59:13 -0500

    affichage des mots

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
+let decembre = 10;
+
 window.onload = function () {
-	console.log("allo");
+	for(let i in contenus) {
+		let jour = i + 1;
+		if(new Date(2023, decembre, jour) < new Date()) {
+			let div = document.createElement("div");
+			div.className = "post-review";
+			div.innerHTML = `
+				<h2 class="post-title"> ${contenus[i].desc}</h2>
+				<p class="post-meta">Créé par les parents le ${jour} décembre 2023</p>
+			`;
+			document.querySelector("#posts").appendChild(div);
+		}
+	}
 }
 
